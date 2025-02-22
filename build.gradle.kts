f600fe4e96ca63f3782dd4120ad2b1f87f88a20e
Seungbeen Wi <wjb127@naver.com>
2025-02-22 15:12:19 +0900

    minor edition

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 } 
